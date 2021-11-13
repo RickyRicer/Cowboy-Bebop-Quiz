6a258600da7588435592b99613634a31a8c321b6
Rick Ansay <rick.ansay@gmail.com>
2021-11-13 16:06:33 -0700

    More hidden pages and javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,8 +8,9 @@ let questionsDivEl = document.getElementById('questions-div');
 let optionsEl = document.getElementById('answer-choices')
 let timeRemaingingEl = document.getElementById('timeremaining');
 let quizTimerId; 
-let quizTime = 90;
-//
+let quizTime = 3;
+//getting the correct value from an object object manipulation object properties
+// CurrentQuestion variable and currentquestion index variable
 
 
 function startQuiz (){
@@ -22,13 +23,42 @@ function startQuiz (){
   timeRemaingingEl.innerHTML = quizTime;
 
   showQuestions();
-  console.log('showQuestions');
+
 };
 //Starts the Quiz
 startButton.onclick = startQuiz;
 
+// questions array
+// questions with answer choices.
+// correct answer.
+const questions = [
+{
+  question: 'What is the name of the ship our bounty hunters are on?',
+  options: ['swordfish', 'red tail', 'hammer head', 'bebop'],
+  answer: 3,
+  },
+  {
+    question: `Who is Spike's friend, turned enemy?`,
+    options: ['Faye', 'Jet', 'Vicious', 'Mao'],
+    answer: 2,
+    },
+    {
+      question: `How does nearly every episode end?`,
+      options: [`3...2...1... Let's Jam!`, `You're Gonna Carry That Weight`, `...See You Space Cowboy`, '...Bang'],
+      answer: 2,
+      },
+      {
+        question: 'Where does the series take place?',
+        options: ['Earth', 'Mars', 'Outer Space...duh', 'The wild, wild west'],
+        answer: 2,
+        },
+]
+
 function showQuestions (){
-  let cu;
+  let currentQuestion = questions[currentQuestionIndex];
+
+  questionsDivEl.children[0].textContent = currentQuestion.question;
+
   for(let i = 0; i < currentQuestion.options.length; i++){
     
     let optionsButton = document.createElement("button");
@@ -36,38 +66,28 @@ function showQuestions (){
     
     optionsEl.appendChild(optionsButton)}
   }
-  
-  // questions array
-  // questions with answer choices.
-  // correct answer.
-const questions = [
-  {
-    question: 'What is the name of the ship our bounty hunters are on?',
-    options: ['swordfish', 'red tail', 'hammer head', 'bebop'],
-    answer: 3,
-    },
-    {
-      question: `Who is Spike's friend, turned enemy?`,
-      options: ['Faye', 'Jet', 'Vicious', 'Mao'],
-      answer: 2,
-      },
-      {
-        question: `How does nearly every episode end?`,
-        options: [`3...2...1... Let's Jam!`, `You're Gonna Carry That Weight`, `...See You Space Cowboy`, '...Bang'],
-        answer: 2,
-        },
-        {
-          question: 'Where does the series take place?',
-          options: ['Earth', 'Mars', 'Outer Space...duh', 'The wild, wild west'],
-          answer: 2,
-          },
-]
 
+function quizEnd (){
+  // stop the quiz time
+  clearInterval(quizTime);
+  timeRemaingingEl.textContent = quizTime;
+
+  //show final page and score 
+  let endDiv = document.getElementById('end-div');
+  endDiv.setAttribute('class','');
+  let finalScore = document.getElementById('final-score');
+  finalScore.textContent = quizTime;
+
+  //Hide Questions
+  questionsDivEl.setAttribute('class', 'hide');
+
+}
 
 function secondHandler(){
   quizTime--;
   timeRemaingingEl.innerHTML = quizTime;
-  //tbd have code here for end quiz also
+  if(quizTime <= 0)
+    quizEnd();
 }
 
 
