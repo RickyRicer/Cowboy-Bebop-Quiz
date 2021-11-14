ef36876c09f5cd563cffc8f0f938d2b5fa1e8ce6
Rick Ansay <rick.ansay@gmail.com>
2021-11-13 22:21:35 -0700

    Created High Score HTML file, added local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,10 +7,12 @@ let startDivEl = document.getElementById('start-div');
 let questionsDivEl = document.getElementById('questions-div');
 let optionsEl = document.getElementById('answer-choices')
 let timeRemaingingEl = document.getElementById('timeremaining');
+let highScoresEl = document.getElementById('high-score');
+let submitBtn = document.getElementById('submit');
 
 let currentQuestionIndex = 0;
 let quizTimerId; 
-let quizTime = 90;
+let quizTime = 10;
 //getting the correct value from an object object manipulation object properties
 // CurrentQuestion variable and currentquestion index variable
 
@@ -68,18 +70,40 @@ function showQuestions (){
     optionsButton.textContent = currentQuestion.options[i];
     
     optionsEl.appendChild(optionsButton)};
+    //Creates an event listener for each answer option
+    optionsEl.children[0].addEventListener('click', function(event){
+      questionChoice(optionsEl.children[0]);
+    });
+    optionsEl.children[1].addEventListener('click', function(event){
+      questionChoice(optionsEl.children[1]);
+    });
+    optionsEl.children[2].addEventListener('click', function(event){
+      questionChoice(optionsEl.children[2]);
+    });
+    optionsEl.children[3].addEventListener('click', function(event){
+      questionChoice(optionsEl.children[3]);
+    });
   }
 
-  
-
+  // Function to check if user selected right or wrong answers
+  function questionChoice (answerChoice){
+    if(answerChoice.textContent != questions[currentQuestionIndex].answer){
+      quizTime -= 10;
+    }
+    currentQuestionIndex++;
+      if(currentQuestionIndex === questions.length)
+    quizStop();
+      else
+    showQuestions();
+  }
   
   function secondHandler(){
     // Countdown
     quizTime--;
     timeRemaingingEl.innerHTML = quizTime;
 
-
     if(quizTime <= 0);
+
     quizStop();
   }
   
@@ -100,6 +124,39 @@ function showQuestions (){
   
   }
 
+
+  function saveHighscore() {
+
+    // make sure value wasn't empty
+    if(initials === ''){ 
+      alert('You must enter your initials!');
+      return;
+    }
+    else if(initials.length > 3){
+      alert('You may only use 3 characters!');
+      return;
+    }
+    else{
+      let highScores;
+    if(JSON.parse(localStorage.getItem('highscores')) != null)
+      highscores = JSON.parse(window.localStorage.getItem('highscores'));
+    else
+      highscores = [];
+
+    let newScore = {
+      initials: initials,
+      score: quizTime,
+    };
+    
+    highscores.push(newScore);
+    
+    localStorage.setItem('highscores', JSON.stringify(highScores));
+    
+    location.href = 'hs.html';
+ };
+}
+
+submitBtn.onclick = saveHighscore;
 
 
 
